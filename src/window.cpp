88a92da60f6c68bc40a7b711540da6e0beff00dc
Brett Vickers <brett@beevik.com>
2019-09-07 11:37:43 -0700

    SDL window scaling added.

--- src/window.cpp
+++ src/window.cpp
@@ -14,8 +14,10 @@
 
 static SDL_Renderer * renderer;
 static SDL_Texture *  texture;
-static SDL_Surface * screen;
-static SDL_Window *  window;
+static SDL_Surface *  screen;
+static SDL_Window *   window;
+
+static SDL_Rect screenrect { 0, 0, 560, 384 };
 
 //===========================================================================
 void WindowDestroy() {
@@ -23,6 +25,10 @@ void WindowDestroy() {
     SDL_DestroyTexture(texture);
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
+    renderer = nullptr;
+    texture  = nullptr;
+    screen   = nullptr;
+    window   = nullptr;
 }
 
 //===========================================================================
@@ -36,18 +42,18 @@ BOOL WindowInitialize() {
         return FALSE;
     }
 
-    SDL_Rect rect { 0, 0, 560, 384 };
-    if (SDL_CreateWindowAndRenderer(rect.w, rect.h, SDL_WINDOW_RESIZABLE, &window, &renderer) != 0) {
+    if (SDL_CreateWindowAndRenderer(screenrect.w * 2, screenrect.h * 2, 0, &window, &renderer) != 0) {
         SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Couldn't create renderer: %s", SDL_GetError());
         return FALSE;
     }
+    SDL_SetWindowFullscreen(window, 0);
 
     texture = SDL_CreateTexture(
         renderer,
         SDL_PIXELFORMAT_ARGB8888,
         SDL_TEXTUREACCESS_STREAMING,
-        rect.w,
-        rect.h
+        screenrect.w,
+        screenrect.h
     );
     if (!texture) {
         SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Couldn't create texture from surface: %s", SDL_GetError());
@@ -55,12 +61,13 @@ BOOL WindowInitialize() {
     }
 
     SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "linear");
-    SDL_RenderSetLogicalSize(renderer, rect.w, rect.h);
+    SDL_RenderSetLogicalSize(renderer, screenrect.w, screenrect.h);
+    SDL_RenderSetIntegerScale(renderer, SDL_TRUE);
 
     screen = SDL_CreateRGBSurface(
         0,
-        rect.w,
-        rect.h,
+        screenrect.w,
+        screenrect.h,
         32,
         0x00ff0000,
         0x0000ff00,
@@ -69,13 +76,13 @@ BOOL WindowInitialize() {
     );
 
     uint32_t * pixels = (uint32_t *)screen->pixels;
-    for (int32_t i = 0, count = rect.w * rect.h; i < count; i += 2) {
+    for (int32_t i = 0, count = screenrect.w * screenrect.h; i < count; i += 2) {
         pixels[i]   = 0xffff0000;
         pixels[i+1] = 0xff000000;
     }
 
     SDL_SetTextureBlendMode(texture, SDL_BLENDMODE_BLEND);
-    SDL_UpdateTexture(texture, nullptr, pixels, rect.w * sizeof(uint32_t));
+    SDL_UpdateTexture(texture, nullptr, pixels, screenrect.w * sizeof(uint32_t));
 
     return TRUE;
 }
@@ -95,6 +102,26 @@ void WindowUpdate() {
         }
         else if (event.type == SDL_KEYDOWN) {
             switch (event.key.keysym.scancode) {
+                case SDL_SCANCODE_1:
+                    if (event.key.keysym.mod & KMOD_CTRL) {
+                        SDL_SetWindowFullscreen(window, 0);
+                        SDL_SetWindowSize(window, screenrect.w, screenrect.h);
+                    }
+                    break;
+
+                case SDL_SCANCODE_2:
+                    if (event.key.keysym.mod & KMOD_CTRL) {
+                        SDL_SetWindowFullscreen(window, 0);
+                        SDL_SetWindowSize(window, 2 * screenrect.w, 2 * screenrect.h);
+                    }
+                    break;
+
+                case SDL_SCANCODE_3:
+                    if (event.key.keysym.mod & KMOD_CTRL) {
+                        SDL_SetWindowFullscreen(window, SDL_WINDOW_FULLSCREEN_DESKTOP);
+                    }
+                    break;
+
                 case SDL_SCANCODE_ESCAPE:
                     mode = MODE_SHUTDOWN;
                     return;
